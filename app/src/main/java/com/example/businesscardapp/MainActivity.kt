24b3eee5adad8a4ffbaba7d27c63b58bb2d58dd5
Mohammed <mo7amxd.7@gmail.com>
2024-06-11 01:19:02 +0100

    real

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.businesscardapp
 
+import android.icu.text.CaseMap.Upper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,7 +11,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -40,7 +40,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UpperInfo(modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.android_logo)
     val colorg = Color(0xFFB9CCB9)
     Box(
         modifier = Modifier
@@ -49,69 +48,102 @@ fun UpperInfo(modifier: Modifier = Modifier) {
 
 
     ) {
+        Moh(
+            modifier = modifier.align(Alignment.Center)
+
+        )
         Column(
-            modifier = modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            modifier = Modifier
+                .padding(bottom = 40.dp, start = 1.dp)
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            Image(
-                painter = image,
-                contentDescription = null,
+            Info(
+                image = painterResource(R.drawable.baseline_local_phone_24),
+                text = "123-456-7890",
                 modifier = modifier
-                    .size(height = 150.dp, width = 150.dp)
-                    .background(
-                        Color(0xFF3A4831)
 
-                    )
             )
-            Text(
-                text = "Jennifer Doe",
-                letterSpacing = 6.sp,
-                fontSize = 28.sp,
-                color = Color.Black,
-                modifier = modifier.padding(top = 4.dp),
-                fontWeight = FontWeight.Light
+            Info(
+                image = painterResource(R.drawable.baseline_share_24),
+                text = "mxd0-0",
+                modifier = modifier
+
             )
-            Text(
-                text = "Android Developer Extraordinaire",
-                fontWeight = FontWeight.Bold,
-                color = Color(0xFF446C2C),
-                modifier = modifier.padding(top = 8.dp)
+            Info(
+                image = painterResource(R.drawable.baseline_mail_24),
+                text = "moh@moh.com",
+                modifier = modifier
+
             )
         }
-        Column(modifier = Modifier.fillMaxHeight().padding(46.dp)
-            .padding(bottom = 50.dp),verticalArrangement = Arrangement.Bottom) {
-        Info(painterResource(R.drawable.baseline_local_phone_24),"+213-777777777")
-        Info(painterResource(R.drawable.baseline_share_24),"@mxd0-0")
-        Info(painterResource(R.drawable.baseline_mail_24),"Moh@moh.com")
-        }
-
 
 
     }
 }
 
 @Composable
-fun Info(image :Painter, text: String, modifier: Modifier = Modifier) {
+fun Info(image: Painter, text: String, modifier: Modifier = Modifier) {
     Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(top = 16.dp),
-        horizontalArrangement = Arrangement.Center
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+
     ) {
         Icon(
-            painter = image,
-            contentDescription = null,
-            modifier.padding(end = 26.dp)
+            painter = image, contentDescription = null,
+            modifier
+                .padding(end = 16.dp)
+                .size(24.dp)
         )
         Text(text, fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
     }
-    
+
+
+}
+
+@Composable
+fun Moh(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.android_logo)
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = modifier
+                .size(height = 150.dp, width = 150.dp)
+                .background(
+                    Color(0xFF3A4831)
+
+                )
+        )
+        Text(
+            text = "Jennifer Doe",
+            letterSpacing = 6.sp,
+            fontSize = 28.sp,
+            color = Color.Black,
+            modifier = modifier.padding(top = 4.dp),
+            fontWeight = FontWeight.Light
+        )
+        Text(
+            text = "Android Developer Extraordinaire",
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF446C2C),
+            modifier = modifier.padding(top = 8.dp)
+        )
+
+    }
 }
 
 
-@Preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun Preview() {
     UpperInfo()
+
+
 }
